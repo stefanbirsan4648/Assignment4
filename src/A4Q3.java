566607859441cf56faf2174d26b60fd86aee0d5c
birss0828 <birss0828@HRH-IC0027339.wrdsb.ca>
2017-03-24 09:22:31 -0400

    New Changes

--- src/A4Q3.java
+++ src/A4Q3.java
@@ -1,8 +1,10 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author birss0828
@@ -13,6 +15,22 @@ public class A4Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // make scanner
+        Scanner input = new Scanner(System.in);
+
+        //input 4 numbers
+        System.out.println("Please enter 4 different numbers on 4 separate lines:");
+        double firstNumber = input.nextDouble();
+        double secondNumber = input.nextDouble();
+        double thirdNumber = input.nextDouble();
+        double fourthNumber = input.nextDouble();
+
+        //user will tell numbers in order
+        System.out.print("The numbers were " + firstNumber + ", ");
+        System.out.print(secondNumber + ",");
+        System.out.print(thirdNumber + ",");
+        System.out.println("and " + fourthNumber + ".");
+
+
     }
 }
